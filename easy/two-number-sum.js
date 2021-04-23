4fd0e40242e73864491e3f7595b0e7019e01322e
Erdal Çay <erdalcay@Erdals-MacBook-Pro.local>
2021-04-23 15:54:47 +0300

    two number sum

---
+++ easy/two-number-sum.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ *  Question: Return the two integer values that adds up to the target sum amongst the distinct values in an array.
+ * 
+ *  @function twoNumberSum
+ *
+ *  @param {Array<number>} nums
+ *    List of distinct integers
+ *  @param {number} targetSum
+ *    Target value to which the two integers we are looking for are expected to sum up to
+ * 
+ *  @return {Array<number>|Array}
+ *    Array of the two integers that add up to the @targetSum
+ *    Or an empty array in case of `not found`
+ * 
+ */
+
+export default function(nums, targetSum) {
+  const visitedNums = {};
+
+  for (const num of nums) {
+    const residual = targetSum - num;
+    if (visitedNums[residual]) return [residual, num];
+    visitedNums[num] = true;
+  }
+
+  return [];
+}
