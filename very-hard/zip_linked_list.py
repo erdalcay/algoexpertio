694f8683703c6af02ca62b962ea123bda01a3775
erdalcay <erdalcay@gmail.com>
2021-09-21 13:29:15 +0300

    Indentation

--- very-hard/zip_linked_list.py
+++ very-hard/zip_linked_list.py
@@ -5,11 +5,10 @@
     A linked list is zipped if its nodes
     are in the following order:
 
-        `1st node -> kth node -> 2nd node -> (k-1)th node`
+    `1st node -> kth node -> 2nd node -> (k-1)th node`
 
     where k is the length of the list.
 """
-
 from __future__ import annotations
 
 
@@ -20,23 +19,23 @@ class LinkedList:
 
 
 def zipLinkedList(head: LinkedList) -> LinkedList:
-	fast = slow = head
-	while fast and fast.next:
-		fast = fast.next.next
-		slow = slow.next
+    fast = slow = head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
 
-	prev = None
-	while slow:
-		slow.next, prev, slow = prev, slow, slow.next
+    prev = None
+    while slow:
+        slow.next, prev, slow = prev, slow, slow.next
 
-	odd_turn = True
-	first, second = head, prev
-	while first.next:
-		if odd_turn:
-			first.next, first = second, first.next
-			odd_turn = False
-		else:
-			second.next, second = first, second.next
-			odd_turn = True
+    odd_turn = True
+    first, second = head, prev
+    while first.next:
+        if odd_turn:
+            first.next, first = second, first.next
+            odd_turn = False
+        else:
+            second.next, second = first, second.next
+            odd_turn = True
 
-  return head
+    return head
